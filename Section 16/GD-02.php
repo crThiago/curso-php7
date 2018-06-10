364bf032b9d1de4ad61c1619ec750c174ce28c5b
Thiago Costa <thiago_8777@hotmail.com>
2018-06-10 03:18:06 -0300

    Criando imagens com biblioteca GD

---
+++ Section 16/GD-02.php
@@ -0,0 +1,16 @@
+<?php 
+
+$image = imagecreatefromjpeg("certificado.jpg");
+
+$titleColor = imagecolorallocate($image, 0, 0, 0);
+$gray = imagecolorallocate($image, 100, 100, 100);
+
+imagestring($image, 5, 450, 150, "CERTIFICADO", $titleColor);
+imagestring($image, 5, 430, 350, "Thiago Rodrigues Costa", $titleColor);
+imagestring($image, 3, 440, 370, utf8_decode("Concluído em: ".date("d/m/Y")), $titleColor);
+
+header("Content-type: image/jpeg");
+
+imagejpeg($image, "certificado-".date("Y-m-d").".jpg");
+
+imagedestroy($image);
---
+++ Section 16/GD-04.php
@@ -0,0 +1,25 @@
+<?php
+
+header("Content-type: image/jpeg");
+
+$file = "wallpaper.jpg";
+
+$newWidth = 256;
+$newHeight = 256;
+
+/*$data = getimagesize($file);
+
+$width = $data[0];
+$height = $data[1];*/
+
+list($oldWidth, $oldHeight) = getimagesize($file);
+
+$newImage = imagecreatetruecolor($newWidth, $newHeight);
+$oldImage = imagecreatefromjpeg($file);
+
+imagecopyresampled($newImage, $oldImage, 0, 0, 0, 0, $newWidth, $newHeight, $oldWidth, $oldHeight);
+
+imagejpeg($newImage);
+
+imagedestroy($oldImage);
+imagedestroy($newImage);
